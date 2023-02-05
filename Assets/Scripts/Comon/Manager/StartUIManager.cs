e3ed4fa0cfd153d1b4ec8e5999612e0a07014824
JhonHe <jhonhe7night@outlook.com>
2023-02-05 14:39:50 +0800

    场景1,UI设置

--- Assets/Scripts/Comon/Manager/StartUIManager.cs
+++ Assets/Scripts/Comon/Manager/StartUIManager.cs
@@ -2,35 +2,55 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartUIManager : MonoBehaviour
-{
+public class StartUIManager : MonoBehaviour {
+    //Main Panel
     private Button startButton;
     private Button settingButton;
-    private Button quitButton;
 
-    private void Start()
-    {
-        startButton = transform.Find("StartBtn").GetComponent<Button>();
-        settingButton = transform.Find("settingBtn").GetComponent<Button>();
-        quitButton = transform.Find("QuitBtn").GetComponent<Button>();
-        
+    private Button quitGameButton;
+
+    //Setting Panel
+    private GameObject settingPanel;
+
+
+    private void Start() {
+        //Main Panel
+        settingPanel = GameObject.Find("SettingPanel");
+        startButton = GameObject.Find("StartBtn").GetComponent<Button>();
+        settingButton = GameObject.Find("SettingBtn").GetComponent<Button>();
+        quitGameButton = GameObject.Find("QuitBtn").GetComponent<Button>();
+
         startButton.onClick.AddListener(ClickStartButton);
         settingButton.onClick.AddListener(ClickSettingButton);
-        quitButton.onClick.AddListener(ClickQuitButton);
+        quitGameButton.onClick.AddListener(ClickQuitGameButton);
+
+
+
+
+        settingPanel.SetActive(false);
     }
 
-    public void ClickStartButton()
-    {
+    /**
+     * 开始
+     */
+    public void ClickStartButton() {
         SceneManager.LoadScene("01");
     }
 
-    public void ClickSettingButton()
-    {
-        
+    /**
+     * 设置
+     */
+    public void ClickSettingButton() {
+        settingPanel.SetActive(true);
     }
 
-    public void ClickQuitButton()
-    {
+    /**
+     * 退出游戏
+     */
+    public void ClickQuitGameButton() {
         Application.Quit();
     }
+
+
+    
 }
