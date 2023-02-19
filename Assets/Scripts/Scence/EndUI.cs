14d5383bf03a5f86612ca1603ddbc90e53ae0c6a
hezhongxian <jhonhe7night@outlook.com>
2023-02-19 19:44:47 +0800

    Release 1.1 添加最终场景UI

---
+++ Assets/Scripts/Scence/EndUI.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndUI : MonoBehaviour {
+    private GameObject gameOverPanel;
+    private Button quickButton;
+    private Button cancelButton;
+
+    void Start() {
+        quickButton = GameObject.Find("QuickButton").GetComponent<Button>();
+        quickButton = GameObject.Find("CancelButton").GetComponent<Button>();
+        quickButton.onClick.AddListener(QuickGame);
+        quickButton.onClick.AddListener(ClickCancel);
+
+        gameOverPanel = GameObject.Find("GameOverPanel");
+        gameOverPanel.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            gameOverPanel.SetActive(true);
+        }
+    }
+
+    public void QuickGame() {
+        Application.Quit();
+    }
+
+    public void ClickCancel() {
+        gameOverPanel.SetActive(false);
+    }
+}
